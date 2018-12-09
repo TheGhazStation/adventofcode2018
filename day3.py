d15efb59d9f3153c096d23560a755e8635294b87
TheGhazStation <theghazstation@gmail.com>
2018-12-08 22:39:13 -0500

    Solve PArt 2 day 3

--- day3.py
+++ day3.py
@@ -6,6 +6,7 @@ def parseline(line):
     global maxX 
     global maxY
     p = line.split('#')[1].split(' ')
+    claim = int(p[0])
     x, y = p[2].replace(':', '').split(',')
     l, w = p[3].split('x')
     x = int(x)
@@ -15,22 +16,31 @@ def parseline(line):
     maxX = max(maxX, x+l)
     maxY = max(maxY, y+w)
 
-    return (x, y, l, w)
+    return (claim, x, y, l, w)
 
-def part1():
+def run():
     with open('data/input_day3_1.txt') as f:
         data = f.read().splitlines()
         lines = list(parseline(l) for l in data)
     
         count = 0
         grid = [[0]*maxX for i in range(maxY)]
-        for (x, y, l, w) in lines:
+        claims = set()
+        for (claim, x, y, l, w) in lines:
+             claims.add(claim)
              for i in range(x, x+l):
-                 for j in range(y, y+w):
-                     grid[i][j] += 1
-                     if grid[i][j] == 2:
-                         count += 1
-        return count
+                for j in range(y, y+w):
+                    if grid[i][j] == 0:
+                        grid[i][j] = claim
+                    elif grid[i][j] != -1:
+                        claims.discard(grid[i][j])
+                        count += 1
+                        grid[i][j] = -1 
+                        claims.discard(claim)
+                    else:
+                        claims.discard(claim)
 
+        print('double claims ' + str(count))
+        print('unique claim ' + str(claims.pop()))
 
-print(part1())
+run()
